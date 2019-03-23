b09ddc5524fca1e0d6d26161d694802a3ccc0394
Yiiiteng <yitwang@ucm.es>
2019-03-23 17:58:48 +0100

    pet actualizado

--- RateMyPet/include/pet.php
+++ RateMyPet/include/pet.php
@@ -1,215 +1,183 @@
 <?php
+require_once __DIR__ . '/Aplicacion.php';
+require_once __DIR__ . '/Usuario.php';
+class Pet{
+        private $petName;
+        private $petId;
+        private $petType;
+        private $petBreed;
+        private $petDescript;
+        private $treats;
 
-require_once "aplicacion.php";
+     public function __construct ($petName,$petId,$petType,$petBreed,$petDescript,$treats){
+        $this->petName = $petName;
+        $this->petId = $petId;
+        $this->petType = $petType;
+        $this->petBreed = $petBreed;
+        $this->petDescript = $petDescript;
+         $this->treats = $treats;
+     }      
+      public static function existePet($idOwner,$petName){
+        $control = Aplicacion::getSingleton();
+        $connect = $control->conexionBd();
+        $sql=sprintf("SELECT petName FROM pets WHERE idOwner = '%s' AND petName = '%s' " 
+            ,$idOwner
+            ,$connect->real_escape_string($petName));
+        $consulta = mysqli_query($connect,$sql);
 
-class Usuario
-{
-
-    public static function login($nombreUsuario, $password)
-    {
-        $user = self::buscaUsuario($nombreUsuario);
-        if ($user && $user->compruebaPassword($password)) {
-            return $user;
-        }
-        return false;
-    }
-
-    public static function buscaUsuario($nombreUsuario)
-    {
-        $app = Aplicacion::getSingleton();
-        $mysqli = $app->conexionBd();
-        $query = sprintf("SELECT * FROM usuario U WHERE U.Nick_Name = '%s'", $mysqli->real_escape_string($nombreUsuario));
-        $rs = $mysqli->query($query);
-        $result = false;
-        if ($rs) {
-            if ( $rs->num_rows == 1) {
-                $fila = $rs->fetch_assoc();
-                $user = new Usuario($fila['Nick_Name'], $fila['ID_Usuario'], $fila['Nombre_Usuario'], $fila['Primer_Apellido'], $fila['Segundo_Apellido'], $fila['Imagen_Perfil'], $fila['Fecha_Nacimiento'], $fila['Correo'], $fila['Telefono'], $fila['Test'], $fila['Contrasena'], $fila['Es_Admin']);
-                $result = $user;
+            if($consulta && $consulta->num_rows == 1) {
+                $consulta->free();
+                return true;
+            }else{
+                return false;
             }
-            $rs->free();
+    }
+    
+      public static function buscarPet($idOwner,$petName){
+        $control = Aplicacion::getSingleton();
+        $connect = $control->conexionBd();
+        $sql=sprintf("SELECT * FROM pets WHERE idOwner = '%s' AND petName = '%s'"
+            ,$idOwner
+            ,$connect->real_escape_string($petName));
+
+        $consulta = mysqli_query($connect,$sql);
+        $result = false;
+
+        if ($consulta) {
+            if ( $consulta->num_rows == 1) {
+                $fila = $consulta->fetch_assoc();
+               // $fruta = new Fruta($fila['id_fruta'], $fila['nombre'],$fila['cantidad'], $fila['precio'],$fila['Oferta']);
+                $result = $fila;
+            }
+            $consulta->free();
         } else {
-            echo "Error al consultar en la BD: (" . $mysqli->errno . ") " . utf8_encode($mysqli->error);
+            echo "Error al consultar en la BD: (" . $connect->errno . ") " . utf8_encode($connect->error);
             exit();
         }
         return $result;
     }
-    
-    public static function crea($Nick_Name, $ID_Usuario, $Nombre_Usuario, $Primer_Apellido, $Segundo_Apellido, $Imagen_Perfil, $Fecha_Nacimiento, $Correo, $Telefono, $Test, $Contrasena, $Es_Admin)
-    {
-        $user = self::buscaUsuario($Nick_Name);
-        if ($user) {
-            return false;
-        }
-        $user = new Usuario($Nick_Name, $ID_Usuario, $Nombre_Usuario, $Primer_Apellido, $Segundo_Apellido, $Imagen_Perfil, $Fecha_Nacimiento, $Correo, $Telefono, $Test, $Contrasena, $Es_Admin);
-        return self::guarda($user);
+
+     public static function insertar($petName,$petType,$petBreed,$petDescript,$treats){
+        
+            $app = Aplicacion::getSingleton();
+            $conn = $app->conexionBd();
+            $query=sprintf("INSERT INTO pets(name,description, type,breed,treats) VALUES('%s', '%s','%s','%s','%s')"
+                   , $conn->real_escape_string($petName)
+                , $conn->real_escape_string($petType)
+                 , $conn->real_escape_string($petBreed)
+                , $conn->real_escape_string($petDescript)
+                , $conn->real_escape_string($treats));
+            if ( $conn->query($query)){
+                return true;
+            }
+             else {
+                echo "Error al insertar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
+                exit();
+            }   
     }
-    
-    private static function hashPassword($password)
-    {
-        return password_hash($password, PASSWORD_DEFAULT);
-    }
-    
-    public static function guarda($usuario)
-    {
-        if ($usuario->ID_Usuario !== null) {
-            return self::actualiza($usuario);
-        }
-        return self::inserta($usuario);
-    }
-    
-    private static function inserta($usuario)
-    {
-        $app = Aplicacion::getSingleton();
-        $mysqli = $app->conexionBd();
-        $query=sprintf("INSERT INTO usuario(Contrasena, Correo, Es_Admin, Fecha_Nacimiento, Nick_Name, Nombre_Usuario, Primer_Apellido, Segundo_Apellido, Telefono, Test) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')"
-            , $mysqli->real_escape_string($usuario->Contrasena)
-            , $mysqli->real_escape_string($usuario->Correo)
-            , '0'
-            , $mysqli->real_escape_string($usuario->Fecha_Nacimiento)
-            , $mysqli->real_escape_string($usuario->Nick_Name)
-            , $mysqli->real_escape_string($usuario->Nombre_Usuario)
-            , $mysqli->real_escape_string($usuario->Primer_Apellido)
-            , $mysqli->real_escape_string($usuario->Segundo_Apellido)
-            , $mysqli->real_escape_string($usuario->Telefono)
-            , '0');
-        if ( $mysqli->query($query) ) {
-            $usuario->ID_Usuario = $mysqli->insert_id;
-        } else {
-            echo "Error al insertar en la BD: (" . $mysqli->errno . ") " . utf8_encode($mysqli->error);
-            exit();
-        }
-        return $usuario;
-    }
-    
-    private static function actualiza($usuario)
-    {
-        $app = Aplicacion::getSingleton();
-        $mysqli = $app->conexionBd();
-        $query=sprintf("UPDATE Usuarios U SET nombreUsuario = '%s', nombre='%s', password='%s', rol='%s' WHERE U.id=%i"
-            , $conn->real_escape_string($usuario->nombreUsuario)
-            , $conn->real_escape_string($usuario->nombre)
-            , $conn->real_escape_string($usuario->password)
-            , $conn->real_escape_string($usuario->rol)
-            , $usuario->id);
-        if ( $mysqli->query($query) ) {
-            if ( $mysqli->affected_rows != 1) {
-                echo "No se ha podido actualizar el usuario: " . $usuario->id;
+
+     public static function eliminar($idOwner,$petName){
+
+            $app = Aplicacion::getSingleton();
+            $conn = $app->conexionBd();
+            $query=sprintf("DELETE FROM pets where idOwner = '%s' AND petName = '%s'"
+                ,$idOwner
+                ,$conn->real_escape_string($petName));
+
+            if ( $conn->query($query) ) {
+                return true;
+            } else {
+                echo "Error al eliminar en la BD: (" . $conn->errno . ") " . utf8_encode($conn->error);
                 exit();
             }
-        } else {
-            echo "Error al insertar en la BD: (" . $mysqli->errno . ") " . utf8_encode($mysqli->error);
-            exit();
+    }
+
+    /*public static function actualizar($id_inventario,$datos){
+
+        $control = Aplicacion::getSingleton();
+        $connect = $control->conexionBd();
+        $productoBD = self::buscarProducto($id_inventario,$datos['nombre']);
+
+
+        if ( $datos['precio'] == NULL) {
+            $datos['precio'] = $productoBD['precio'];
+        }
+
+        if ( $datos['cantidad'] == NULL){
+             $datos['cantidad'] = $productoBD['cantidad'];
         }
         
-        return $usuario;
-    }
+        $sql=sprintf("UPDATE productos SET  cantidad = '%s', precio = '%s' WHERE id_inventario = '%s' AND nombre = '%s'"
+            ,$connect->real_escape_string($datos['cantidad'])
+            ,$connect->real_escape_string($datos['precio'])
+            ,$id_inventario
+            ,$connect->real_escape_string($datos['nombre']));
+
+        $consulta = mysqli_query($connect,$sql);
+
+        if($consulta)
+            return true;
+        else{
+            echo "Error al actualizar la BD: (" . $connect->errno . ") " . utf8_encode($connect->error);
+        return false;
+        }
     
-    private $Nick_Name;
+    }*/
+     
+     public static function allPets($idOwner){
+        $control = Aplicacion::getSingleton();
+        $connect = $control->conexionBd();
+        $sql=sprintf("SELECT * FROM pets  WHERE idOwner = '%s'" ,$idOwner);
+         $rs = $connect->query($sql);
 
-    private $ID_Usuario;
-
-    private $Nombre_Usuario;
-
-    private $Primer_Apellido;
-
-    private $Segundo_Apellido;
-
-    private $Imagen_Perfil;
-
-    private $Fecha_Nacimiento;
-
-    private $Correo;
-
-    private $Telefono;
-
-    private $Test;
-
-    private $Contrasena;
-
-    private $Es_Admin;
+            if($rs){
+                $producto = array();
+                while($row =  $rs->fetch_assoc()){
+                    $producto[] = $row;
+                }
+                $rs->free();
+                
+                return $producto;
+                    
+            }else {
+                    echo "Error al consultar la BD: (" . $connect->errno . ") " . utf8_encode($connect->error);
+                    exit();
+                }
 
 
+     }
+     
 
-    private function __construct($Nick_Name, $ID_Usuario, $Nombre_Usuario, $Primer_Apellido, $Segundo_Apellido, $Imagen_Perfil, $Fecha_Nacimiento, $Correo, $Telefono, $Test, $Contrasena, $Es_Admin)
-    {   
-        $this->Nick_Name = $Nick_Name;
-        $this->ID_Usuario = $ID_Usuario;
-        $this->Nombre_Usuario = $Nombre_Usuario;
-        $this->Primer_Apellido = $Primer_Apellido;
-        $this->Segundo_Apellido = $Segundo_Apellido;
-        $this->Imagen_Perfil = $Imagen_Perfil;
-        $this->Fecha_Nacimiento = $Fecha_Nacimiento;
-        $this->Correo = $Correo;
-        $this->Telefono = $Telefono;
-        $this->Test = $Test;
-        $this->Contrasena = $Contrasena;
-        $this->Es_Admin = $Es_Admin;
-    }
+         public function petName()
+        {
+            return $this->petName;
+        }
 
+        public function petId()
+        {
+            return $this->petId;
+        }
 
+        public function petType()
+        {
+            return $this->petType;
+        }
 
+        public function petBreed()
+        {
+            return $this->petBreed;
+        }
 
-    public function ID_Usuario()
-    {
-        return $this->ID_Usuario;
-    }
+        public function petDescript()
+        {
+            return $this->petDescript;
+        }
 
+         public function treats()
+        {
+            return $this->treats;
+        }
 
-    public function Nombre_Usuario()
-    {
-        return $this->Nombre_Usuario;
-    }
-
-    public function Primer_Apellido()
-    {
-        return $this->Primer_Apellido;
-    }
-
-    public function Segundo_Apellido()
-    {
-        return $this->Segundo_Apellido;
-    }
-
-    public function Imagen_Perfil()
-    {
-        return $this->Imagen_Perfil;
-    }
-
-    public function Fecha_Nacimiento()
-    {
-        return $this->Fecha_Nacimiento;
-    }
-
-    public function Correo()
-    {
-        return $this->Correo;
-    }
-
-    public function Telefono()
-    {
-        return $this->Telefono;
-    }
-
-    public function Test()
-    {
-        return $this->Test;
-    }
-
-    public function Es_Admin()
-    {
-        return $this->Es_Admin;
-    }
-
-    public function compruebaPassword($password)
-    {
-        $Contrasena = self::hashPassword($this->Contrasena);
-        return password_verify($password, $Contrasena);
-    }
-
-    public function cambiaPassword($nuevoPassword)
-    {
-        $this->password = self::hashPassword($nuevoPassword);
-    }
 
 }
+?>
--- RateMyPet/include/procesarRegistroPet.php
+++ RateMyPet/include/procesarRegistroPet.php
@@ -1,44 +1,20 @@
 <?php
-	session_start();
-	require_once "includes/config.php";
-    require_once "includes/pet.php";
+
+    require_once "Pet.php";
 
 	 
-		$nickName = isset($_POST['usuario']) ? $_POST['usuario'] : null;
-		$contrasenia = isset($_POST['password']) ? $_POST['password'] : null;
-		$nombre = isset($_POST['nomReg']) ? $_POST['nomReg'] : null;
-		$primerApellido = isset($_POST['ape1Reg']) ? $_POST['ape1Reg'] : null;
-		$segundoApellido = isset($_POST['ape2Reg']) ? $_POST['ape2Reg'] : null;
-		$fechaNac = isset($_POST['birthdate']) ? $_POST['birthdate'] : null;
-		$correo = isset($_POST['mail']) ? $_POST['mail'] : null;
-		$telefono = isset($_POST['mobile']) ? $_POST['mobile'] : null;
+		$petName = isset($_POST['pet']) ? $_POST['pet'] : null;
+		$petType = isset($_POST['petType']) ? $_POST['petType'] : null;
+		$petBreed = isset($_POST['petBreed']) ? $_POST['petBreed'] : null;
+		$petDescript = isset($_POST['petDescript']) ? $_POST['petDescript'] : null;
 
 
-		if(empty($nickName) or empty($contrasenia) or empty($nombre) or empty($primerApellido) or empty($fechaNac) or empty($correo))
-			header("Location: ../errorRegistro.php");
+		if(empty($pet)  or empty($petBreed) or empty($petDescript) or empty($petType))
+			//header("Location: ../errorRegistro.php");
+			echo "Debes rellenar todos los campos";
 
 		else{
-			$usuario = Usuario::crea($nickName, null, $nombre, $primerApellido, $Segundo_Apellido, null,  $fechaNac, $correo, $telefono, 0, $contrasenia, 0);
-			if (! $usuario ) {
-		    	header("Location: ../errorRegistro.php");
-			} else {
-				$_SESSION["LogIn"] = TRUE;
-				$_SESSION["username"] = $nickName;
-				$_SESSION["idUser"] = $usuario->ID_Usuario();
-				$_SESSION["nombre"] = $usuario->Nombre_Usuario();
-				$_SESSION["primerApellido"] = $usuario->Primer_Apellido();
-				$_SESSION["segundoApellido"] = $usuario->Segundo_Apellido();
-				$_SESSION["imagenPerfil"] = $usuario->Imagen_Perfil();
-				$_SESSION["fechaNacimiento"] = $usuario->Fecha_Nacimiento();
-				$_SESSION["email"] = $usuario->Correo();
-				$_SESSION["telefono"] = $usuario->Telefono();
-				$_SESSION["testCompletado"] = $usuario->Test();
-				if($usuario->Es_Admin() == '1')
-    				$_SESSION['esAdmin'] = true;
-				header('Location: ../index.php');
-				exit();
-
-			}
+			$pet = Pet:: insertar($petName,$petType,$petBreed,$petDescript,0);
 		 }
 	
 
