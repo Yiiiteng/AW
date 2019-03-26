617aefb3a81c7405f25e957a938ba44b5c433ac9
MiniNanook <adruiz01@ucm.es>
2019-03-26 13:48:42 +0100

    Updated Perfil Owner

--- RateMyPet/perfilOwner.php
+++ RateMyPet/perfilOwner.php
@@ -6,91 +6,91 @@
 <head>
 	<title>Rate My Pet - perfil</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="css/perfil.css">
+	<link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/content.css">
 </head>
-<body class="color-fondo">
 	<?php
 		require("include/comun/header.php");
 	?>
 
-	<div class="card" id="owner">
-		<div class="infoOwner">
+	<div>
+		<?php
+			$_SESSION["ownerOpet"]="owner";
 
-			<div class="content-img">
-				<?php
-					$_SESSION["ownerOpet"]="owner";
+			$path='usuarios/'.$_SESSION["username"].'.jpg';
+			if (file_exists('usuarios/'.$_SESSION["username"].'.jpg')) {
+				echo '<img src='.$path.' alt="Logo" width="100" height="100" />';
+			}
+			else{
+				echo '<img src="usuarios/default.png" alt="Logo" width="100" height="100" />';
+			}
+		?>
 
-					$path='usuarios/'.$_SESSION["username"].'.png';
-					if (file_exists('usuarios/'.$_SESSION["username"].'.png')) {
-						echo '<img src='.$path.' alt="Logo" width="100" height="100" />';
-					}
-					else{
-						echo '<img src="usuarios/default.png" alt="Logo" width="100" height="100" />';
-					}
-				?>
+		<form class="file" action="include/procesarFichero.php" method="POST" enctype="multipart/form-data">
+			Change foto(jpg/png): 
+			<input type="file" name="file" accept="image/*" id="upload" >
+			<input type="submit" value="Change">
+		</form>  
 
-	            <form class="file" action="include/procesarFichero.php" method="POST" enctype="multipart/form-data">
-	            	Change foto(jpg/png): 
-	                <input type="file" name="file" accept="image/*" id="upload" >
-	                <input type="submit" value="Change">
-	            </form>  
-	        </div>
+		<table id="info">
+		<?php
+			echo "<tr>
+			<th> ".$_SESSION['username']."</th>
+			</tr>
+			<tr>
+			<th>".$_SESSION['email']."</th>
+			</tr>";
+		?>
+		<tr>
+		<!--echo $_SESSION['followed']; need add a function-->
+		<th>48 seguidores</th>
+		</tr>
+		</table>
 
-			<table id="info">
-				<?php
-				echo "<tr>
-						<th> ".$_SESSION['username']."</th>
-					</tr>
-					<tr>
-						<th>".$_SESSION['email']."</th>
-					</tr>";
-				?>
-				
-				<tr>
-					<!--echo $_SESSION['followed']; need add a function-->
-				<th>48 seguidores</th>
-				</tr>
-			</table>
 
-			<div id="rankingPerfil">
-				<table>
-					<tr>
-						<!--echo $_SESSION['weekrank'];-->
-						<th>Current Weekly Rank: #302</th>
-					</tr>
-					<tr>
-						<!--echo $_SESSION['bestrank'];-->
-						<th>Best Weekly Rank: #3</th>
-					</tr>
-				</table>
-			</div>
-		</div>
+		<table>
+		<tr>
+		<!--echo $_SESSION['weekrank'];-->
+		<th>Current Weekly Rank: #302</th>
+		</tr>
+		<tr>
+		<!--echo $_SESSION['bestrank'];-->
+		<th>Best Weekly Rank: #3</th>
+		</tr>
+		</table>
 
-	</div>
-
-	<div class="card" id="myPet">
+		<div class="card" id="myPet">
 		<h3>My pets</h3>
 		<?php
-			$control = Aplicacion::getSingleton();
-			$conn = $control->conexionBd();
-			$sql = sprintf("SELECT * FROM pets WHERE owner_id = '%s'", $_SESSION['owner_id']); // Return owner id
-			$result = $conn->query($sql);
-		
-			if ($result->num_rows > 0) {
-				// output data of each row
-				while($row = $result->fetch_assoc()) {
-					//$dir="usuarios/". $_SESSION['username']."/".$row['name']."/".$row['name'].".png";
-					//echo 'Owner of: ' .$row["idPet"] .'<img src='.$dir.' alt="petFoto" width="100" height="100" />';
-					$pet = $row["idPet"];
-					echo '<a href="perfilPet.php?idPet='.$pet.'">Go To Profile of '.$row['name'].'</a><br/>';
-				}
-			} else {
-				echo "You don't own any pets!";
+		$control = Aplicacion::getSingleton();
+		$conn = $control->conexionBd();
+		$sql = sprintf("SELECT * FROM pets WHERE owner_id = '%s'", $_SESSION['owner_id']); // Return owner id
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+				//$dir="usuarios/". $_SESSION['username']."/".$row['name']."/".$row['name'].".png";
+				//echo 'Owner of: ' .$row["idPet"] .'<img src='.$dir.' alt="petFoto" width="100" height="100" />';
+				$pet = $row["idPet"];
+				echo '<a href="perfilPet.php?idPet='.$pet.'"><img src="img/Adrian.jpg">Go To Profile of '.$row['name'].'</a><br/>';
+				/*echo '<form method="post" action="perfilPet.php">
+				<input type="hidden" name="idPet" value='.$row["idPet"].'>
+				<input type="submit">
+				<label>Go to '.$row['name'].'\'s  Profile</label>
+				</form>';*/
 			}
+		} else {
+			echo "You don't own any pets!";
+		}
 
 		?>
 		<button type="button" id="button-add-pet" onclick="window.location.href='addPet.php'"> + </button>
 	</div>
 
+	<?php
+		require("include/comun/footer.php");
+	?>
 </body>
 </html>
