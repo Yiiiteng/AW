cffd3405a850cb4605fcb339b3bc0c5b8ec725c9
Atalajacalareja <gummyg974@gmail.com>
2019-04-11 11:40:57 +0200

    Update selectHome.php

--- RateMyPet/include/selectHome.php
+++ RateMyPet/include/selectHome.php
@@ -3,7 +3,7 @@ require_once __DIR__.'/config.php';
 $postList = array();
 
 if (isset($_SESSION['username'])) { // if logged in
-    $sql = 'SELECT P.idpost, P.time, P.likes, P.repets, P.petid, P.description, pets.name FROM followedpets FP join posts P on P.petId = FP.petId join pets on pets.idPet = P.petId WHERE '.$_SESSION['owner_id'].' = FP.userId'; // Return the user
+    $sql = 'SELECT P.idpost, P.time, P.likes, P.repets, P.petid, P.description, pets.name FROM followedpets FP join posts P on P.petId = FP.petId join pets on pets.idPet = P.petId WHERE '.$_SESSION['owner_id'].' = FP.userId ORDER BY P.time ASC'; // Return the user
     $postList = $conn->query($sql);
     //hacer esto luego $postList = $data->fetch_assoc();
     if(!$postList) {
