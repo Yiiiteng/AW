786dc46b579ee1e6be7c454dcdc6191dad98132d
MiniNanook <adruiz01@ucm.es>
2019-05-15 18:47:28 +0200

    Delete changeImg.php

--- RateMyPet/include/changeImg.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo '<form class="file" action="include/procesarFichero.php" method="POST" enctype="multipart/form-data">
-		<input  type="submit" name="imageField" accept="image/*" id="upload" >
-		<input value="Change">
-			</form>';
-
-?>
