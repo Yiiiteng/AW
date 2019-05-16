a18847d44dcb2288d3147d86603cc3ae5f1882c1
Yifei Liu <37576347+luojuee@users.noreply.github.com>
2019-05-16 19:19:38 +0200

    Delete borrarPet.php

--- borrarPet.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once __DIR__.'/config.php';
-	require_once __DIR__.'/Pet.php';
-
-	$pet = Pet::buscarPet($_GET['idpet']);
-	$idowner=$pet->owner_id();
-	$pet->borrarMascota();
-	header('Location: ../ownerProfile.php?id='.$idowner.'');
-?>
